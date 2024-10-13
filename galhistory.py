11d5c9b5764e05c25bb5436d5dd3f56fcbeb869e
KaioGroot <85202894+KaioGroot@users.noreply.github.com>
2024-10-12 21:23:38 -0400

    Update galhistory.py

--- galhistory.py
+++ galhistory.py
@@ -14,7 +14,7 @@ try:
       st.html('''<script>
                      let comandos = document.getElementsByClassName('comandos');
                      for(let i = 0; i < comandos.length; i++){
-                       alert(i)
+                       console.log(i)
                        }
                 </script>''')
 except FileNotFoundError:
