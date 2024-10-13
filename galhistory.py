2f6518f2f57d5bcf3dbfdb07fb17f116ab96c3bf
KaioGroot <85202894+KaioGroot@users.noreply.github.com>
2024-10-12 21:27:19 -0400

    Update galhistory.py

--- galhistory.py
+++ galhistory.py
@@ -11,10 +11,10 @@ try:
       #st.markdown("---")
       st.write(historia[i]["historia"],i)
       st.markdown("---")
-st.html('''
+st.html(''''
 <script>
  alert('ioo')
                      
-</script>''')
+</script>''''')
 except FileNotFoundError:
   st.error("Ocorreu um erro, arquivo não encontrado: ", FileNotFoundError)
